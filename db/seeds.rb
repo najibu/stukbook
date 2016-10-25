3a5335d8d5b0e3d4fa692d98da581647fa9ec7f1
najibu <ns.najibu@gmail.com>
2016-10-25 15:54:17 +0100

    Added database seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "nsubuga", email: "ns.najibu@gmail.com", password: "password", password_confirmation: "password")
+User.create(username: "nsubuga2", email: "ns.najibu2@gmail.com", password: "password", password_confirmation: "password")
+User.create(username: "nsubuga3", email: "ns.najibu3@gmail.com", password: "password", password_confirmation: "password")
+User.create(username: "nsubuga4", email: "ns.najibu4@gmail.com", password: "password", password_confirmation: "password")
+User.create(username: "nsubuga5", email: "ns.najibu5@gmail.com", password: "password", password_confirmation: "password")
+
+p "Test users created"
