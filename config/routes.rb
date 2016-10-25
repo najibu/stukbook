72d9e2a4c1815e9e2e7c99f9436b6934f31851f7
najibu <ns.najibu@gmail.com>
2016-10-25 15:03:39 +0100

    Added extra user attributes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show]
-
   devise_for :users, controllers: {registrations: 'registrations'}
   root 'pages#home'
+  resources :users, only: [:show]
 
 end
