2e72a75ee4b87c3ee1683f9304dcd63c0710c244
najibu <ns.najibu@gmail.com>
2016-10-25 15:42:36 +0100

    Added users index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'registrations'}
   root 'pages#home'
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
 
 end
